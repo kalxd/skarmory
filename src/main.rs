fa1d40b8425de87fc3d469ad9d2016dd7af76737
荀洪道 <kalxd@126.com>
2024-11-16 15:40:25 +0800

    多任务。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::{thread, time::Duration};
+use tokio::{runtime, time};
+
+mod state;
+
+async fn task_timer(i: usize) {
+	let mut tid = time::interval(Duration::from_secs(10));
+	loop {
+		tid.tick().await;
+
+		println!("doing {i}");
+	}
+}
+
+fn create_timer_task() {
+	let rt = runtime::Builder::new_current_thread()
+		.enable_time()
+		.build()
+		.unwrap();
+
+	rt.block_on(task_timer(1));
+}
+
+fn create_web_task() {
+	let rt = runtime::Builder::new_multi_thread()
+		.enable_all()
+		.build()
+		.unwrap();
+	rt.block_on(task_timer(2));
+}
+
 fn main() {
-    println!("Hello, world!");
+	let timer = thread::spawn(create_timer_task);
+	let web = thread::spawn(create_web_task);
+
+	timer.join().unwrap();
+	web.join().unwrap();
 }
---
+++ src/state.rs
@@ -0,0 +1,9 @@
+#[derive(Debug)]
+pub struct JandanPic {
+	link: String,
+}
+
+#[derive(Debug)]
+pub struct AppState {
+	pics: Vec<JandanPic>,
+}
