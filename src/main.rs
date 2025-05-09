bbb749e4412fe0004b0dc3e04782f65a567fe3cf
荀洪道 <kalxd@126.com>
2025-05-09 13:01:47 +0800

    添加一大堆依赖。

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,3 @@
-use futures::future;
-use state::AppState;
-use std::time::Duration;
-use tokio::{runtime, time};
-
-mod error;
-mod spider;
-mod state;
-mod web;
-
-async fn create_timer_task(state: AppState) {
-	let mut tid = time::interval(Duration::from_secs(60 * 30));
-	loop {
-		tid.tick().await;
-		if let Err(e) = spider::create_spider_task(state.clone()).await {
-			eprintln!("{e}")
-		}
-	}
-}
-
 fn main() {
-	let rt = runtime::Builder::new_multi_thread()
-		.enable_all()
-		.build()
-		.unwrap();
-
-	let state = state::create_init_state();
-
-	rt.block_on(async move {
-		let timer = create_timer_task(state.clone());
-		let web = web::create_web_task(state);
-		future::join(timer, web).await;
-	});
+	println!("hello world");
 }
