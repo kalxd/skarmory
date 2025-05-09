c9efe52e7d9f0ba55d50de6d79a6d656162a8c60
荀洪道 <kalxd@126.com>
2025-05-09 14:13:08 +0800

    连接到数据库。

--- src/base.rs
+++ src/base.rs
@@ -1,13 +1,16 @@
 use serde::Deserialize;
+use sqlx::{PgPool, postgres::PgConnectOptions};
 
 #[derive(Debug)]
 pub enum AppError {
+	DBErr(String),
 	BootErr(String),
 }
 
 impl std::fmt::Display for AppError {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		match self {
+			Self::DBErr(s) => write!(f, "DB Err: {s}"),
 			Self::BootErr(s) => write!(f, "{s}"),
 		}
 	}
@@ -21,11 +24,18 @@ impl From<config::ConfigError> for AppError {
 	}
 }
 
+impl From<sqlx::Error> for AppError {
+	fn from(value: sqlx::Error) -> Self {
+		Self::DBErr(value.to_string())
+	}
+}
+
 pub type Result<T, E = AppError> = std::result::Result<T, E>;
 
 #[derive(Debug, Deserialize)]
 pub struct FileConfigDatabase {
 	host: String,
+	port: u16,
 	user: String,
 	password: String,
 	database: String,
@@ -50,5 +60,16 @@ impl FileConfig {
 		};
 
 		config.build()?.try_deserialize().map_err(Into::into)
+	}
+
+	pub async fn connect_to_db(&self) -> Result<PgPool> {
+		let option = PgConnectOptions::new()
+			.host(&self.database.host)
+			.port(self.database.port)
+			.username(&self.database.user)
+			.password(&self.database.password)
+			.database(&self.database.database);
+
+		PgPool::connect_with(option).await.map_err(Into::into)
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use base::Result;
 #[ntex::main]
 async fn main() -> Result<()> {
 	let config = base::FileConfig::read_default_config()?;
+	let pool = config.connect_to_db().await?;
 
 	dbg!(config);
+	dbg!(pool);
 	Ok(())
 }
